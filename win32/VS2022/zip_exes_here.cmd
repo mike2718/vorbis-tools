c9db2407618ee38d3b21d0ce8ab2994f984d4242
Mike Akiba <mike2718@gmail.com>
2024-09-07 13:46:19 +0800

    使用LIBFLAC

--- win32/VS2022/zip_exes_here.cmd
+++ win32/VS2022/zip_exes_here.cmd
@@ -1,10 +1,16 @@
 @echo off
 cd /d "%~dp0"
+
 mkdir .\vorbis-tools-1.4.2-win32-bin
+del /q .\vorbis-tools-1.4.2-win32-bin\*.dll .\vorbis-tools-1.4.2-win32-bin\*.exe
 copy /y .\Release\*.exe .\vorbis-tools-1.4.2-win32-bin\
-upx .\vorbis-tools-1.4.2-win32-bin\*.exe
-del /q vorbis-tools-1.4.2-win32-bin.zip vorbis-tools-1.4.2-win32-bin.7z
-zip -r vorbis-tools-1.4.2-win32-bin.zip vorbis-tools-1.4.2-win32-bin
-7z a -t7z vorbis-tools-1.4.2-win32-bin.7z vorbis-tools-1.4.2-win32-bin
+copy /y ..\..\..\libogg\bin\ogg.dll .\vorbis-tools-1.4.2-win32-bin\
+copy /y ..\..\..\vorbis\bin\vorbis.dll .\vorbis-tools-1.4.2-win32-bin\
+copy /y ..\..\..\vorbis\bin\vorbisfile.dll .\vorbis-tools-1.4.2-win32-bin\
+copy /y ..\..\..\FLAC\bin\FLAC.dll .\vorbis-tools-1.4.2-win32-bin\
+
+cd /d vorbis-tools-1.4.2-win32-bin
+del /q vorbis-tools-1.4.2-win32-bin.7z
+7z a -t7z vorbis-tools-1.4.2-win32-bin.7z *.exe *.dll LICENSE
 
 pause
